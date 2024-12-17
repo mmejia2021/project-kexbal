8eac32e77a62b7672223e329fb71697940320164
Melany <melany_mejiar@bantrab.com>
2024-12-17 07:34:17 -0600

    actualizacion de home version anterior estable

--- src/app/modules/landing/pages/home/home.component.ts
+++ src/app/modules/landing/pages/home/home.component.ts
@@ -4,8 +4,6 @@ import { Router, RouterLink } from '@angular/router';
 import { NgClass, NgFor, NgIf } from '@angular/common';
 import { initFlowbite } from 'flowbite';
 import { ApiService } from '../../../../services/api.service';
-import { DataTable, convertJSON } from "simple-datatables";
-import { TableRow } from '../../../../core/models/tableRow';
 
 @Component({
   selector: 'app-home',
@@ -17,12 +15,14 @@ import { TableRow } from '../../../../core/models/tableRow';
 export class HomeComponent implements OnInit {
 
   isShowTable = false;
-  //isShowCalculadora = true;
+  isShowCalculadora = true;
   form!: FormGroup;
   submitted = false;
-  ofertas: any[] = [];  // Aquí se guardan las ofertas obtenidas
-  tableData: any[] = []; // Aquí se guardan los datos transformados para la tabla
-
+  ofertas: any;
+  currentPage = 1; // Página actual
+  itemsPerPage = 5; // Elementos por página (default)
+  paginatedData = []; // Datos visibles en la página actual
+  limit = 6; // Número máximo de elementos por página
   resultado: number | null = 0.00;
   monto1: number | null = 0.00;
   monto2: number | null = 0.00;
@@ -34,43 +34,40 @@ export class HomeComponent implements OnInit {
       ofertaUsa: ['', [Validators.required]],
       tasaGt: ['', Validators.required],
     });
+
     initFlowbite();
 
-    this.apiService.get('publications').subscribe((data) => {
-      // Transform JSON data into rows for the table
-      this.ofertas = data.activeOffers
-
-       // Transformamos los datos a un formato adecuado para DataTable
-       this.tableData = this.ofertas.map((item: any) => [
-        item.id_seller.username,  // Primera columna: Usuario
-        item.available_offer,     // Segunda columna: USD disponible
-        item.kexbal_exchange_rate, // Tercera columna: Tasa de cambio
-        item.bank                 // Cuarta columna: Banco
-      ]);
-
-      console.log(this.tableData); // Muestra el nuevo formato
-      let newData = {
-        headings: ["Heading 1", "Heading 2", "Heading 3", "Heading 4", "Heading 5"],
-        data: this.tableData
-    };
-      console.log(newData)
-      const dataTable = new DataTable("#myTable",{
-        labels: {
-          perPage: "registros por página", // Selector de registros por página
-          noRows: "No se encontraron registros", // Mensaje cuando no hay datos
-          info: "Mostrar {start} - {end} de {rows} registros", // Información de paginación
-        },
-        searchable: false,
-        fixedHeight: true,
-        perPage: 6,
+    setTimeout(() => {
+      this.apiService.get('publications').subscribe(data => {
+        this.ofertas = data.activeOffers.slice(0, 6)
+        console.log(data.activeOffers);
       });
-      dataTable.insert(newData)
 
-    });
+    }, 2000);
+
   }
 
+
+  calcularResultado() {
+    this.monto1 = this.form.value.ofertaUsa || 0.00;
+    this.monto2 = this.form.value.tasaGt || 0.00;
+
+    const monto1 = this.form.value.ofertaUsa || 0.00;
+    const monto2 = this.form.value.tasaGt || 0.00;
+
+    this.resultado = monto1 * monto2; // Cambia esta lógica según lo que necesites calcular
+  }
+
+
+
+  toggleDisplay() {
+    this.isShowTable = false;
+    this.isShowCalculadora = true;
+  }
+  toggleDisplayCalculate() {
+    this.isShowTable = true;
+    this.isShowCalculadora = false;
+  }
+
+
 }
-
-
-
-
